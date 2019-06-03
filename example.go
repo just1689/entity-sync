c8f45110349f8e0ab78a8e2f35f738fe18982851
Justin Tamblyn <just1689@gmail.com>
2019-06-03 12:35:05 +0200

    Named workers and specified listen addr for fun and better example

--- example.go
+++ example.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/just1689/entity-sync/bridge"
 	"github.com/just1689/entity-sync/dq"
 	"github.com/just1689/entity-sync/shared"
+	"github.com/just1689/entity-sync/web"
 	"github.com/sirupsen/logrus"
+	"math/rand"
 	"net"
 	"net/http"
 )
@@ -14,10 +18,17 @@ const entityType shared.EntityType = "items"
 
 var GlobalBridge *bridge.Bridge
 
+var Name string
+var role = flag.Int("role", 1, "1 for reader, 2 for changer")
+var listenLocal = flag.String("listen", ":8080", "listen addr: :8080")
+
 func main() {
+	flag.Parse()
+
+	resolveName()
 
 	mux := http.NewServeMux()
-	l, err := net.Listen("tcp", ":8080")
+	l, err := net.Listen("tcp", *listenLocal)
 	if err != nil {
 		logrus.Fatalln(err)
 	}
@@ -31,11 +42,19 @@ func main() {
 	//Ensure the bridge will send NSQ messages for entityType to onNotify
 	GlobalBridge.Subscribe(entityType)
 
-	HandleEntity(mux, topic)
+	web.HandleEntity(mux)
 
 	err = http.Serve(l, mux)
 	if err != nil {
 		panic(err)
 	}
 
+}
+
+func resolveName() {
+	if *role == 1 {
+		Name = fmt.Sprint("Reader", rand.Intn(100))
+	} else if *role == 2 {
+		Name = fmt.Sprint("Mutator", rand.Intn(100))
+	}
 }
