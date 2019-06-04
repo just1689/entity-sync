f75e48201b0f0b21d559001a995832fd2476f46d
Just1689 <Just1689@gmail.com>
2019-06-04 21:57:34 +0200

    refactor

--- esnsq/durable-queueing.go
+++ esnsq/durable-queueing.go
@@ -1,48 +1,18 @@
 package esnsq
 
 import (
-	"fmt"
-	"github.com/bitly/go-nsq"
 	"github.com/just1689/entity-sync/shared"
-	uuid "github.com/satori/go.uuid"
-	"github.com/sirupsen/logrus"
 )
 
 //BuildPublisher can be called to give a method that in turn can be called to create publishers
-func BuildPublisher(nsqAddr string) shared.EntityHandler {
+func BuildPublisher(addr string) shared.EntityHandler {
 	return func(entityType shared.EntityType) shared.ByteHandler {
-		return GetNSQProducer(nsqAddr, entityType)
+		return GetNSQProducer(addr, entityType)
 	}
 }
 
-func GetNSQProducer(nsqAddr string, entityType shared.EntityType) shared.ByteHandler {
-	config := nsq.NewConfig()
-	w, _ := nsq.NewProducer(nsqAddr, config)
-	producer := func(msg []byte) {
-		err := w.Publish(entityType.GetQueueName(), msg)
-		if err != nil {
-			logrus.Panic("Could not publish to NSQ ", nsqAddr, "on topic", entityType)
-		}
-	}
-	return producer
-}
-
-func BuildSubscriber(nsqAddr string) shared.EntityByteHandler {
+func BuildSubscriber(addr string) shared.EntityByteHandler {
 	return func(entityType shared.EntityType, callback shared.ByteHandler) {
-		SubscribeNSQ(nsqAddr, entityType, callback)
+		SubscribeNSQ(addr, entityType, callback)
 	}
-}
-
-func SubscribeNSQ(nsqAddr string, entityType shared.EntityType, f shared.ByteHandler) {
-	config := nsq.NewConfig()
-	q, _ := nsq.NewConsumer(entityType.GetQueueName(), fmt.Sprint(uuid.NewV4().String(), "#ephemeral"), config)
-	q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
-		f(message.Body)
-		return nil
-	}))
-	err := q.ConnectToNSQD(nsqAddr)
-	if err != nil {
-		logrus.Panic("Could not connect to NSQ for subscribe", nsqAddr)
-	}
-
 }
---
+++ esnsq/nsq.go
@@ -0,0 +1,35 @@
+package esnsq
+
+import (
+	"fmt"
+	"github.com/just1689/entity-sync/shared"
+	"github.com/nsqio/go-nsq"
+	uuid "github.com/satori/go.uuid"
+	"github.com/sirupsen/logrus"
+)
+
+func GetNSQProducer(nsqAddr string, entityType shared.EntityType) shared.ByteHandler {
+	config := nsq.NewConfig()
+	w, _ := nsq.NewProducer(nsqAddr, config)
+	producer := func(msg []byte) {
+		err := w.Publish(entityType.GetQueueName(), msg)
+		if err != nil {
+			logrus.Panic("Could not publish to NSQ ", nsqAddr, "on topic", entityType)
+		}
+	}
+	return producer
+}
+
+func SubscribeNSQ(nsqAddr string, entityType shared.EntityType, f shared.ByteHandler) {
+	config := nsq.NewConfig()
+	q, _ := nsq.NewConsumer(entityType.GetQueueName(), fmt.Sprint(uuid.NewV4().String(), "#ephemeral"), config)
+	q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		f(message.Body)
+		return nil
+	}))
+	err := q.ConnectToNSQD(nsqAddr)
+	if err != nil {
+		logrus.Panic("Could not connect to NSQ for subscribe", nsqAddr)
+	}
+
+}
