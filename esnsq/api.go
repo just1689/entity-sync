5b2bca43b74637b3c86b4de92e1ba7b042202299
Just1689 <Just1689@gmail.com>
2019-06-04 22:02:55 +0200

    docs

--- esnsq/api.go
+++ esnsq/api.go
@@ -4,6 +4,12 @@ import (
 	"github.com/just1689/entity-sync/shared"
 )
 
+/*
+	If you would like to use something other than NSQ, you can provide your own implementation of
+	`shared.AddressableEntityHandler` and `shared.AddressableEntityByteHandler`.
+	This must be provided to `esbridge.BuildBridge()`
+*/
+
 //BuildPublisher can be called to give a method that in turn can be called to create publishers
 var BuildPublisher shared.AddressableEntityHandler = func(addr string) shared.EntityHandler {
 	return func(entityType shared.EntityType) shared.ByteHandler {
