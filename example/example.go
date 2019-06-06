ffaaaf6d8a22394ed0c7c9d8a95afae894fa2911
Just1689 <Just1689@gmail.com>
2019-06-06 18:39:22 +0200

    example

--- example/example.go
+++ example/example.go
@@ -35,10 +35,10 @@ func main() {
 	es := entitysync.Setup(config)
 
 	//Register an entity and tell the library how to fetch and what to write to the client
-	es.RegisterEntityAndDBHandler(entityType, func(rowKey shared.EntityKey, pusher shared.ByteHandler) {
-		item := fetch(rowKey)
+	es.RegisterEntityAndDBHandler(entityType, func(entityKey shared.EntityKey, secret string, handler shared.ByteHandler) {
+		item := fetch(entityKey)
 		b, _ := json.Marshal(item)
-		pusher(b)
+		handler(b)
 	})
 
 	//Simulate data changes
